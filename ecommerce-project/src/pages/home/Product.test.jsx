7b3512edf1040f4a8e62d1a05e2786eaa895cd41
rahul <devararahul56@gmail.com>
2025-09-04 11:10:09 -0500

    use beforeEach to remove duplication

--- ecommerce-project/src/pages/home/Product.test.jsx
+++ ecommerce-project/src/pages/home/Product.test.jsx
@@ -1,15 +1,20 @@
-import { it, expect, describe,vi  } from 'vitest'
+import { it, expect, describe,vi, beforeEach  } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import {Product} from './Product'
 import axios from 'axios'
 
-vi.mock('axios', () => {})
+vi.mock('axios')
+
+
 
 describe('Product component', () => {
-  it('displays the product details correctly', () => {
+  let product;
 
-    const product = {
+  let loadcart
+
+  beforeEach(() => {
+    product=  {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
@@ -21,10 +26,13 @@ describe('Product component', () => {
     keywords: ["socks", "sports", "apparel"]
   }
 
-  const loadcart = vi.fn()
+  loadcart = vi.fn()
+  })
+  it('displays the product details correctly', () => {
+
     render(<Product product={product} loadCart={loadcart}/>)
 
-   expect( screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')).toBeInTheDocument();
+    expect( screen.getByText('Black and Gray Athletic Cotton Socks - 6 Pairs')).toBeInTheDocument();
     expect( screen.getByText('$10.90')).toBeInTheDocument();
     expect( screen.getByText('87')).toBeInTheDocument();
     expect(
@@ -37,34 +45,21 @@ describe('Product component', () => {
   })
 
   it('adds a product to the cart ',async() =>{
-    const product = {
-    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87
-    },
-    priceCents: 1090,
-    keywords: ["socks", "sports", "apparel"]
-  }
-
-  const loadcart = vi.fn()
     render(<Product product={product} loadCart={loadcart}/>)
 
     const user = userEvent.setup()
    const addToCartButton = (screen.getByRole('button', {name: 'Add to Cart'})) 
    await user.click(addToCartButton)
 
-   expect(axios.post).toHaveBeenCalledWith()('/api/cart-items',{
+   expect(axios.post).toHaveBeenCalledWith(
+    '/api/cart-items',
+    {
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 1
-   } )
+   } 
+  )
 
-   expect(loadCart).toHaveBeenCalled();
+   expect(loadcart).toHaveBeenCalled();
 
   })
-
-
-  
 })
